1910aa6ec6119c1ca1c3abb7c987fda29177cf03
Denys <denyscheremis@gmail.com>
2022-02-02 21:36:49 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+/* if (4 == 8) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+} */
+
+/* const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Error');
+} else {
+    console.log('OK!');
+}
+
+(num === 50) ? console.log('OK!') : console.log('Error'); */
+
+/* const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+    case 100:
+        console.log('неверно');
+        break;
+    case 50:
+        console.log('в точку');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+} */
+
+/* let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+}
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+    break;
+    continue;
+    }
+    console.log(i);
+} */
